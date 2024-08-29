3291088b4539d2b657ee25620aaca081b7893b77
Cy_Wilson <youremail@example.com>
2024-08-29 09:49:36 +0800

    added sub info and implement fetch  data for info

--- pokemon.js
+++ pokemon.js
@@ -18,17 +18,41 @@ async function fetchData() {
     }
 
     const data = await response.json();
+
+    //Pokemon Image
     const pokemonSprite = data.sprites.front_default;
     const imgElement = document.getElementById("pokemonImage");
 
     imgElement.src = pokemonSprite;
     imgElement.style.display = "block";
 
+    //Pokemon Name
     const pokemonNameElement = document.getElementById("name");
     pokemonNameElement.textContent = data.name;
 
+    //Pokemon Type
     const pokemonTypeElement = document.getElementById("pokemonType");
     pokemonTypeElement.textContent = data.types[0].type.name;
+
+    //Pokemon Sub-Information
+    const pokemonInformation = document.querySelector(".pokemonInfo");
+    pokemonInformation.style.display = "block";
+
+    //Pokemon Name
+    const pokeName = document.getElementById("pokeName");
+    pokeName.textContent = data.name.toUpperCase();
+
+    //Pokemon Type
+    const pokeType = document.getElementById("pokeType");
+    pokeType.textContent = data.types[0].type.name;
+
+    //Pokemon id
+    const pokeId = document.getElementById("idNumber");
+    pokeId.textContent = `#${data.id.toString().padStart(3, "0")}`;
+
+    //Pokemon Height
+    const pokeHeight = document.getElementById("pokemonHeight");
+    pokeHeight.textContent = data.height;
   } catch (error) {
     console.error(error);
     alert("Pokemon doesn't exist");
